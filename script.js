4226f9b7b111b07015acfdfa3fdf730bd9eed97e
jaimeamn1 <jaimeamn1@gmail.com>
2022-11-13 16:19:39 -0500

    agregar fecha a las tareas

--- script.js
+++ script.js
@@ -3,16 +3,35 @@ import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
+const addTask =(evento) => {
+  const list = document.querySelector('[data-list]');
+  const task = createTask(evento);
+  list.appendChild(task);
+};
+
+const taskList = [];
+
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
+  const calendar = document.querySelector("[data-form-date")
   const value = input.value;
-  const list = document.querySelector('[data-list]');
+  const date = calendar.value
+  const dateFormat = moment(date).format('DD/MM/YYYY')
+  console.log(dateFormat)
   const task = document.createElement('li');
   task.classList.add('card');
   input.value = '';
   //backticks
   const taskContent = document.createElement('div');
+
+  const taskObj = {
+    value,
+    dateFormat
+  };
+
+  taskList.push(taskObj)
+  localStorage.setItem('tasks', JSON.stringify(taskList))
 
   const titleTask = document.createElement('span');
   titleTask.classList.add('task');
@@ -20,11 +39,13 @@ const createTask = (evento) => {
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
   // task.innerHTML = content;
-
+  const dateElement = document.createElement('span');
+  dateElement.innerHTML = dateFormat
   task.appendChild(taskContent);
+  task.appendChild(dateElement)
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
 //Arrow functions o funciones anonimas
-btn.addEventListener('click', createTask);
+btn.addEventListener('click', addTask);
